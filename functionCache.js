e5cd4589f937b7c0ff0b93ec2e000a45fe9ced4e
yp <yp@example.com>
2024-09-16 06:15:57 +0100

    0.04

--- functionCache.js
+++ functionCache.js
@@ -2,12 +2,11 @@ const farmhash = require('farmhash')
 const { stringifyFlatted } = require('./helper')
 
 class FunctionCache {
-
-    constructor(options) {        
+    constructor(options) {
         this.timeouts = {}
         this.data = {}
         this.setOptions(null, true)
-        if(options) this.setOptions(options)
+        if (options) this.setOptions(options)
     }
 
     /**
@@ -20,23 +19,25 @@ class FunctionCache {
      * @property {string} delete_event_name - name of event to emit on delete
      */
     setOptions(options, init) {
-        if (!options) options = {
-            set_callback: null,
-            delete_callback: null,
-            set_event_name: 'function_cache_set',
-            delete_event_name: 'function_cache_delete',
-        }
+        if (!options)
+            options = {
+                set_callback: null,
+                delete_callback: null,
+                set_event_name: 'function_cache_set',
+                delete_event_name: 'function_cache_delete',
+            }
         for (let name in options) {
             this.setOption(name, options[name], init)
         }
     }
-    
+
     setOption(name, value, init) {
-        if(!init && this[name] === undefined) return
-        const required_type = name.indexOf('callback') !== -1 ? 'function' : 'string'
+        if (!init && this[name] === undefined) return
+        const required_type =
+            name.indexOf('callback') !== -1 ? 'function' : 'string'
         if (typeof value === required_type) this.name = value
     }
-    
+
     /**
      * Get the result of a function from the cache. If the result does not exist in the cache, call the function and cache the result.
      * @param {function} func - the function to call if the result is not in the cache
@@ -48,15 +49,28 @@ class FunctionCache {
      */
     async get(func, args, ttl, no_freeze, callback) {
         this.setOptions('set_callback', callback)
-        if(!args || !Array.isArray(args)) args = []
+        if (!args || !Array.isArray(args)) args = []
         const key1 = farmhash.hash32(func.toString())
-        const key2 = args.length ? farmhash.hash32(stringifyFlatted(args)) : 'empty'
-        
-        if(this.data[key1] !== undefined && this.data[key1][key2] !== undefined) return Promise.resolve(this.data[key1][key2].value)
-        let result = func(...args)
-        if(result instanceof Promise) result = await result
+        const key2 = args.length
+            ? farmhash.hash32(stringifyFlatted(args))
+            : 'empty'
 
-        if(this.set_callback) this.set_callback(this.set_event_name, [key1, key2, result, ttl || 0, Boolean(no_freeze)])         
+        if (
+            this.data[key1] !== undefined &&
+            this.data[key1][key2] !== undefined
+        )
+            return Promise.resolve(this.data[key1][key2].value)
+        let result = func(...args)
+        if (result instanceof Promise) result = await result
+
+        if (this.set_callback)
+            this.set_callback(this.set_event_name, [
+                key1,
+                key2,
+                result,
+                ttl || 0,
+                Boolean(no_freeze),
+            ])
 
         this.setWithKeys(key1, key2, result, ttl, no_freeze)
         return Promise.resolve(result)
@@ -71,14 +85,14 @@ class FunctionCache {
      * @param {boolean} [no_freeze] - if true, do not freeze the value in the cache
      */
     setWithKeys(key1, key2, result, ttl, no_freeze) {
-        if(!this.data[key1]) this.data[key1] = {}
-        this.data[key1][key2] = {value: result}
-        if(!no_freeze) Object.freeze(this.data[key1][key2])
-        if(ttl && this.timeouts[key1] === undefined) {
+        if (!this.data[key1]) this.data[key1] = {}
+        this.data[key1][key2] = { value: result }
+        if (!no_freeze) Object.freeze(this.data[key1][key2])
+        if (ttl && this.timeouts[key1] === undefined) {
             this.timeouts[key1] = setTimeout(() => {
                 this.delete(key1, null, true)
             }, ttl)
-        }        
+        }
     }
 
     /**
@@ -91,17 +105,23 @@ class FunctionCache {
     delete(k, args, dont_clear_timeout, callback) {
         this.setOptions('delete_callback', callback)
         const _type = typeof k
-        const key1 = _type !== 'string' && _type !== 'number' ? (_type === 'function' ? farmhash.hash32(k.toString()) : farmhash.hash32(stringifyFlatted(k))) : k
-        
+        const key1 =
+            _type !== 'string' && _type !== 'number'
+                ? _type === 'function'
+                    ? farmhash.hash32(k.toString())
+                    : farmhash.hash32(stringifyFlatted(k))
+                : k
+
         const key2 = args ? farmhash.hash32(stringifyFlatted(args)) : null
-        if(this.data[key1] !== undefined) {
-            if(key2) {                
-                delete this.data[key1][key2]		
+        if (this.data[key1] !== undefined) {
+            if (key2) {
+                delete this.data[key1][key2]
                 return
             }
             delete this.data[key1]
         }
-        if(this.delete_callback && (key2 || !dont_clear_timeout)) this.delete_callback(this.delete_event_name, [key1, key2])        
+        if (this.delete_callback && (key2 || !dont_clear_timeout))
+            this.delete_callback(this.delete_event_name, [key1, key2])
         this.clearTimeout(key1, dont_clear_timeout)
     }
 
@@ -111,21 +131,21 @@ class FunctionCache {
      * @param {string} [key2] - the second part of the cache key
      */
     deleteWithKeys(key1, key2) {
-        if(this.data[key1] !== undefined) {
-            if(key2) {
+        if (this.data[key1] !== undefined) {
+            if (key2) {
                 delete this.data[key1][key2]
                 return
             }
             delete this.data[key1]
         }
-        this.clearTimeout(key1)        
+        this.clearTimeout(key1)
     }
 
     clearTimeout(key1, dont_clear_timeout) {
-        if(this.timeouts[key1] !== undefined) {
-            if(!dont_clear_timeout) clearTimeout(this.timeouts[key1])
+        if (this.timeouts[key1] !== undefined) {
+            if (!dont_clear_timeout) clearTimeout(this.timeouts[key1])
             delete this.timeouts[key1]
-        }        
+        }
     }
 }
 
